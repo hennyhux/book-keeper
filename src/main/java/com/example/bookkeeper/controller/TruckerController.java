7853f57ef08b0dcd6a6b63781c76457b17f1fe9e
Henry <hennyhux@gmail.com>
2023-04-13 17:41:03 -0400

    Reorg

--- src/main/java/com/example/bookkeeper/controller/TruckerController.java
+++ src/main/java/com/example/bookkeeper/controller/TruckerController.java
@@ -3,10 +3,13 @@ package com.example.bookkeeper.controller;
 import com.example.bookkeeper.scribe.ActivityScribe;
 import com.example.bookkeeper.scribe.ActivityScribeLoggingLevels;
 import com.keeper.openapi.api.PetsApi;
+import com.keeper.openapi.model.Pet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TruckerController implements PetsApi {
@@ -20,8 +23,21 @@ public class TruckerController implements PetsApi {
     @Override
     public ResponseEntity<Void> createPets(){
         ResponseEntity<Void> responseEntity = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
-        scribe.logEntry(this.getClass(), "DIZZY NUTS!", ActivityScribeLoggingLevels.INFO);
+        scribe.logEntry(this.getClass(), "Create pets endpoint invoked in controller", ActivityScribeLoggingLevels.INFO);
         return responseEntity;
     }
 
+    @Override
+    public ResponseEntity<List<Pet>> listPets(Integer limit){
+        ResponseEntity<List<Pet>> responseEntity = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        scribe.logEntry(this.getClass(), "List pets endpoint invoked in controller", ActivityScribeLoggingLevels.INFO);
+        return responseEntity;
+    }
+
+    @Override
+    public ResponseEntity<List<Pet>> showPetById(String petID){
+        ResponseEntity<List<Pet>> responseEntity = new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        scribe.logEntry(this.getClass(), "Show pets endpoint invoked in controller", ActivityScribeLoggingLevels.INFO);
+        return responseEntity;
+    }
 }
